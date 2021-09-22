b9b9b07e4a4f0c3fd6a13c24a24cd3a5a567c88d
SebastianMacaya <contacto@sebastianmacaya.com>
2021-09-22 11:22:04 -0300

    elimino boton de comprar catalogo

--- js/carteras.js
+++ js/carteras.js
@@ -26,7 +26,6 @@ const pintarCards = (data) => {
   data.forEach((item) => {
     templateCard.querySelector("h5").textContent = item.title;
     templateCard.querySelector("p").textContent = `$${item.price}`;
-    templateCard.querySelector("button").dataset.id = item.id;
     templateCard.querySelector("img").setAttribute("src", item.thumbnailUrl);
 
     const clone = templateCard.cloneNode(true);
