e3eb8ea933d908c5c35023d0d27cfefd429f1950
Guido31s <guidomanusejas@gmail.com>
2021-09-30 10:00:00 -0300

    add js, html de portamacetas y tapices

---
+++ js/tapices.js
@@ -0,0 +1,35 @@
+const cards = document.getElementById("cards");
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+
+// Eventos
+// El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
+document.addEventListener("DOMContentLoaded", (e) => {
+  fetchData();
+});
+
+/* ------------ Traer productos (Reemplazo Js vanilla por Jquery) ----------- */
+const fetchData = async () => {
+  const URLGET = "../data/tapices.json";
+  $.get(URLGET, async function (respuesta, estado) {
+    if (estado === "success") {
+      let misDatos = respuesta;
+      const data = await misDatos;
+      pintarCards(data);
+    }
+  });
+};
+
+/* ---------------------------- Pintar productos ---------------------------- */
+const pintarCards = (data) => {
+  data.forEach((item) => {
+    templateCard.querySelector("h5").textContent = item.title;
+    templateCard.querySelector("p").textContent = `$${item.price}`;
+    templateCard.querySelector("img").setAttribute("src", item.thumbnailUrl);
+
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
